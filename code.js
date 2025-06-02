a73494fa130bfe13b8054a8848373cf2b49bafcb
Ivan Kliuev <45692534+sorumond@users.noreply.github.com>
2025-06-02 13:59:20 +0300

    update burger
    CU-86c3u1jvc

--- code.js
+++ code.js
@@ -6,3 +6,11 @@ const header = document.querySelector("#header");
 burgerButton.addEventListener("click", () => {
   header.classList.toggle("showBurgerMenu");
 });
+
+const burgerMenuLinks = document.querySelectorAll("#burgerNav a");
+
+burgerMenuLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    header.classList.remove("showBurgerMenu");
+  });
+});
